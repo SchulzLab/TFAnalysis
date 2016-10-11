0bccb7e10bb2d78c30a42d02d05c393e7bb8b4c9
Florian Schmidt <fschmidt@srv-13-245.mpi-inf.mpg.de>
2016-10-11 15:59:47 +0200

    Added three scripts neccessary for the preprocessing of the data. The scripts do not have any hardcoded paths left, they have a help function for the arguments and comments in the beginning of the script explain what the scripts are doing

---
+++ Preprocessing/Prepare_TEPIC_Output_For_Intersection.py
@@ -0,0 +1,70 @@
+import os
+import sys
+import argparse
+from sets import Set
+
+
+#This script transforms the TEPIC output into bed file like structure.
+#Further, the coverage computed within peaks is added as an additional column to the file containing the unscaled scores.
+#
+#
+#arg1: Path to the folder containing TF predictions computed by TEPIC. The reformated files will be stored in the same directory.
+
+def main():
+	parser=argparse.ArgumentParser(prog="Intersect_Bins_And_TF_Predictions.py")
+	parser.add_argument("TF_Predictions",nargs=1,help="Path to TEPIC TF predictions computed in DHS. The reformated files will be stored in the same directory. Note that the original files will not be deleted.")
+	args=parser.parse_args()
+
+	files=os.listdir(args.TF_Predictions[0])
+	for f in files:
+		if ("Affinity.txt" in f):
+			print("Reformating "+str(f))
+			original=open(args.TF_Predictions[0]+f,"r")
+			normalised=open(args.TF_Predictions[0]+f.replace(".txt",".bed"),"w")
+			header=original.readline()
+			normalised.write("#\t\t"+header)
+			for l in original:
+				s=l.split()
+				ch=s[0].split(":")[0]
+				se=s[0].split(":")[1]
+				start=se.split("-")[0]
+				end=se.split("-")[1]
+				newline=str("chr"+ch+"\t"+start+"\t"+end)
+				for i in xrange(1,len(s)):
+					newline+="\t"+str(s[i])
+				newline+="\n"
+				normalised.write(newline)
+			normalised.close()
+			original.close()
+		if ("Peak_Coverage.txt" in f):
+			print("Reformating "+str(f))
+			original=open(args.TF_Predictions[0]+f,"r")
+			normalised=open(args.TF_Predictions[0]+f.replace(".txt",".bed"),"w")
+			normalised.write("#\t\t\tCoverage\n")
+			for l in original:
+				s=l.split()
+				newline="chr"+s[0]+'\t'+s[1]+'\t'+s[2]+'\t'+s[3]+'\n'
+				normalised.write(newline)
+			normalised.close()
+			original.close()
+
+	files=os.listdir(args.TF_Predictions[0])
+	tfs=Set()
+	for f in files:
+		tfs.add(f.split("_")[0])
+	for tf in tfs:
+		for f in files:
+			if (tf in f):
+				if ("Scaled_Affinity.bed" in f):
+					continue
+				elif ("Affinity.bed" in f):
+					command1="cut -f 4 "+args.TF_Predictions[0]+tf+"*Peak_Coverage.bed > coverage.temp"
+					print(command1)
+					os.system(command1)
+					command="paste "+args.TF_Predictions[0]+f+" coverage.temp > "+args.TF_Predictions[0]+tf+"_Affinity_Peak_Coverage.bed"	
+					print(command)
+					os.system(command)
+					command="rm coverage.temp"
+					os.system(command)
+			
+main()
---
+++ Preprocessing/Split_and_Balance_ChIP-seq_TSV_files.py
@@ -0,0 +1,54 @@
+import os
+import subprocess
+import sys
+import argparse
+
+#This script transforms the original ChIPSeq TSV files into
+#bed files separated per TF and tissue.
+#The number of bound samples is not reduced. The number of unbound
+#samples is reduced to the number of present bound samples.
+#
+#
+#Arguments:
+#arg1: Path to the ChIP-seq tsv files provided at synapse
+#arg2: Path to write the balanced files to, splitted per TF and Tissue.
+
+def main():
+	parser=argparse.ArgumentParser(prog="Split_and_Balance_ChIP-seq_TSV_files.py")                                                                                                               
+	parser.add_argument("source",nargs=1,help="Path to the ChIP-seq tsv files provided at synapse")
+	parser.add_argument("destination",nargs=1,help="Path to write the balanced files to, splitted per TF and Tissue")
+	args=parser.parse_args() 
+
+	command="mkdir "+str(args.destination[0])
+	subprocess.call(command,shell=True)
+	
+	ChIP_Labels=os.listdir(args.source[0])
+	for f in ChIP_Labels:
+		infile=open(args.source[0]+f,"r")
+		header=infile.readline()
+		tissues=header.split()[3:]
+		counter=4
+		infile.close()
+		for t in tissues:
+			print f.split(".")[0]+" in Tissue "+t
+			command="awk '{if ($"+str(counter)+"== \"B\") print $0}' "+args.source[0]+f+" | wc -l "
+			output=subprocess.check_output(command,shell=True)
+			command="awk '{if ($"+str(counter)+"== \"U\") print $0}' "+args.source[0]+f+" | shuf  > Negative_Temp.txt" 
+			subprocess.call(command,shell=True)
+			command="head -n "+str(output).strip()+" Negative_Temp.txt > Negative_Temp_Cutted.txt"
+			subprocess.call(command,shell=True)
+			command="awk '{if ($"+str(counter)+"== \"B\") print $0}' "+args.source[0]+f+" > Positive_Temp.txt"
+			subprocess.call(command,shell=True)
+			command="cat Positive_Temp.txt Negative_Temp_Cutted.txt | cut -f 1,2,3,"+str(counter)+" > "+args.destination[1]+str(f.split(".")[0])+"."+t+".Balanced.bed"
+			subprocess.call(command,shell=True)
+			command="rm Positive_Temp.txt"
+			subprocess.call(command,shell=True)
+			command="rm Negative_Temp.txt"
+			subprocess.call(command,shell=True)
+			command="rm Negative_Temp_Cutted.txt"
+			subprocess.call(command,shell=True)
+			counter+=1
+
+
+
+main()
