6f48e097da7c3c80a851e868c68eefeadb5bbc59
Florian Schmidt <fschmidt@srv-13-245.mpi-inf.mpg.de>
2017-01-30 09:55:36 +0100

    Added does2unix to account for the changed file endings of the labels for the benchmarked data

--- Preprocessing/Split_and_Balance_ChIP-seq_TSV_files.py
+++ Preprocessing/Split_and_Balance_ChIP-seq_TSV_files.py
@@ -16,7 +16,7 @@ def main():
 
 	command="mkdir "+str(args.destination[0])
 	subprocess.call(command,shell=True)
-	
+		
 	ChIP_Labels=os.listdir(args.source[0])
 	for f in ChIP_Labels:
 		infile=open(args.source[0]+f,"r")
@@ -27,8 +27,11 @@ def main():
 		infile.close()
 		for t in tissues:
 			print f.split(".")[0]+" in Tissue "+t
+			command="dos2unix "+args.source[0]+f
+			subprocess.call(command,shell=True)
 			command="awk '{if ($"+str(counter)+"== \"B\") print $0}' "+args.source[0]+f+" | wc -l "
 			output=subprocess.check_output(command,shell=True)
+			print("Number of bound samples "+str(output))
 			command="awk '{if ($"+str(counter)+"== \"U\") print $0}' "+args.source[0]+f+" | shuf  > Negative_Temp.txt" 
 			subprocess.call(command,shell=True)
 			command="head -n "+str(output).strip()+" Negative_Temp.txt > Negative_Temp_Cutted.txt"
